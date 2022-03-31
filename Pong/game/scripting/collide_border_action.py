0c51c5e40ddec1554446ab02e3f3f7ee21ecefdc
Ethan Chipman <ethandchipman@gmail.com>
2022-03-30 20:33:50 -0600

    ball stops on edge

--- Pong/game/scripting/collide_border_action.py
+++ Pong/game/scripting/collide_border_action.py
@@ -6,7 +6,7 @@ from game.scripting.action import Action
 class CollideBordersAction(Action):
 
     def __init__(self, physics_service):
-        self._physics_service = physics_service  
+        self._physics_service = physics_service 
         
     def execute(self, cast, script, callback):
         ball = cast.get_first_actor(BALL_GROUP)
@@ -16,11 +16,11 @@ class CollideBordersAction(Action):
         y = position.get_y()
 
                 
-        # if x < FIELD_LEFT:
-        #     ball.bounce_x()
+        if x < FIELD_LEFT:
+            callback.change_scene("menu", cast, script)
 
-        # elif x >= (FIELD_RIGHT - BALL_WIDTH):
-        #     ball.bounce_x()
+        elif x >= (FIELD_RIGHT - BALL_WIDTH):
+            callback.change_scene("menu", cast, script)
 
         if y < FIELD_TOP:
             ball.bounce_y()
